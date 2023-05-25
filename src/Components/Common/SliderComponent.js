37be124766e0f5042db3c464ed4151f77fd7456d
hkagei <k_dawg_1022@hotmail.com>
2023-05-24 23:35:55 -0600

    touch up on slider component, create result.js sliderselect.js tenureselect.js

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -16,7 +16,31 @@ const SliderComponent = ({
   }) => {
     return (
       <Stack my={1.4}>
-  
+        <Stack gap={1}>
+          <Typography variant="subtitle2">{label}</Typography>
+          <Typography variant="h5">
+          {unit} {amount}
+          </Typography>
+        </Stack>
+        <Slider
+          min={min}
+          max={max}
+          defaultValue={defaultValue}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+          onChange={onChange}
+          value={value}
+          marks
+          step={steps}
+        />  
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="caption" color="text.secondary">
+          {unit} {min}
+          </Typography>
+          <Typography variant="caption" color="text.secondary">
+          {unit} {max}
+          </Typography>
+        </Stack>
       </Stack>
     )
   }
