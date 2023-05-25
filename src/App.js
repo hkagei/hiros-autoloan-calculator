e5c934ca01dc3d7617e606cd0395bbcd2e49d244
hkagei <k_dawg_1022@hotmail.com>
2023-05-24 23:50:32 -0600

    go

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import Navbar from "./Components/Navbar";
-// import Result from "./Components/Result";
-// import SliderSelect from "./Components/SliderSelect";
-// import TenureSelect from "./Components/TenureSelect";
+import Result from "./Components/Result";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect";
 
 function App() {
+
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
+
+  console.log(data)
+
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="xl" sx={{marginTop:4}}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
-            {/* <SliderSelect />
-            <TenureSelect /> */}
+            <SliderSelect data={data} setData={setData} />
+            <TenureSelect data={data} setData={setData}/>
           </Grid>
           <Grid item xs={12} md={6}>
-              {/* <Result/> */}
-            </Grid>
+            <Result data={data}/>
+            
           </Grid>
-        </Container>
+        </Grid>
+      </Container>
     </div>
   );
 }
