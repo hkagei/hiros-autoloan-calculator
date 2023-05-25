8e73b17348dff90303645f1064c26fd5b2b6e08b
hkagei <k_dawg_1022@hotmail.com>
2023-05-24 23:41:44 -0600

    write TenureSelect.js

--- src/Components/TenureSelect.js
+++ src/Components/TenureSelect.js
@@ -10,7 +10,23 @@ const TenureSelect = ({ data, setData }) => {
     };
   
     return (
-        
+        <FormControl fullWidth>
+    <InputLabel id="demo-simple-select-label">Tenure</InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      value={data.loanTerm}
+      label="Tenure"
+      defaultValue={5}
+      onChange={handleChange}
+    >
+      <MenuItem value={5}>5 years</MenuItem>
+      <MenuItem value={10}>10 years</MenuItem>
+      <MenuItem value={15}>15 years</MenuItem>
+      <MenuItem value={20}>20 years</MenuItem>
+      <MenuItem value={25}>25 years</MenuItem>
+    </Select>
+  </FormControl>
     )
   };
   
